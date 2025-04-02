6bf4d6c284e3338eea6ab5487a12534d992c4aae
ze-pena <jose.pena.dev@gmail.com>
2025-04-01 21:27:17 -0300

    feat: create new component "listar-pensamento"

---
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  standalone: false,
+  templateUrl: './listar-pensamento.component.html',
+  styleUrl: './listar-pensamento.component.css'
+})
+export class ListarPensamentoComponent {
+
+}
