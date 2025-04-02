b57fc76aeb513a27080239855225cc34f1dacffc
ze-pena <jose.pena.dev@gmail.com>
2025-04-01 21:27:03 -0300

    feat: create new component "criar-pensamento"

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  standalone: false,
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+  pensamento = {
+    id: "1",
+    conteudo: "Aprendendo Angular",
+    autoria: "Dev",
+    modelo: "modelo1"
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!")
+  }
+
+  cancelar() {
+    alert("Ação cancelada!")
+  }
+}
