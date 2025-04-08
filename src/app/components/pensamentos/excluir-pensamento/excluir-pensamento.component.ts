a1c2dadfbe7ac890c3f28e1e1e67fd67d209761d
ze-pena <jose.pena.dev@gmail.com>
2025-04-07 23:41:31 -0300

    feat: create new "excluir-pensamento" component

---
+++ src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  standalone: false,
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrl: './excluir-pensamento.component.css'
+})
+export class ExcluirPensamentoComponent {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscar(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento() {
+    if (this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listar-pensamento'])
+      })
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listar-pensamento'])
+  }
+}
