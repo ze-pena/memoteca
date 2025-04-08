2bb1603f5d2733f496c5d7183add04becea09207
ze-pena <jose.pena.dev@gmail.com>
2025-04-07 23:43:03 -0300

    feat: add new path routes to edit or delete content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   { path: "",
@@ -16,6 +18,14 @@ const routes: Routes = [
   {
     path: "listar-pensamento",
     component: ListarPensamentoComponent
+  },
+  {
+    path: "pensamentos/editar-pensamento/:id",
+    component: EditarPensamentoComponent
+  },
+  {
+    path: "pensamentos/excluir-pensamento/:id",
+    component: ExcluirPensamentoComponent
   }
 ];
 
