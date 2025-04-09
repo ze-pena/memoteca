0da331e680209c31361ced98ebf27302205203a7
ze-pena <jose.pena.dev@gmail.com>
2025-04-09 18:10:19 -0300

    feat: create new "mudar-favorito" method to set itens as favorite

--- src/app/components/pensamentos/pensamento.service.ts
+++ src/app/components/pensamentos/pensamento.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
@@ -11,8 +11,17 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API)
+  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+    let params = new HttpParams()
+      .set('_page', pagina)
+      .set('_limit', itensPorPagina)
+
+    if (filtro.trim().length > 2) {
+      params = params.set('q', filtro)
+    }
+
+    return this.http.get<Pensamento[]>(this.API, { params })
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
@@ -22,6 +31,11 @@ export class PensamentoService {
   editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`
     return this.http.put<Pensamento>(url, pensamento)
+  }
+
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito;
+    return this.editar(pensamento);
   }
 
   excluir(id: number): Observable<Pensamento> {
