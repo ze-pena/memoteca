1c8dfd0ab964bfffb9f630ec3aced35fbcef5c61
ze-pena <jose.pena.dev@gmail.com>
2025-05-06 15:51:30 -0300

    feat: create and configure new "reuse-stategy" routing-rule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
 
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
+import { CustomReuseStrategy } from './custom-reuse-strategy';
 
 const routes: Routes = [
   { path: "",
@@ -17,7 +18,10 @@ const routes: Routes = [
   },
   {
     path: "listar-pensamento",
-    component: ListarPensamentoComponent
+    component: ListarPensamentoComponent,
+    data: {
+      reuseComponent: true,
+    }
   },
   {
     path: "pensamentos/editar-pensamento/:id",
@@ -30,7 +34,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: CustomReuseStrategy }
+  ],
 })
 export class AppRoutingModule { }
